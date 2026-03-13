3b97f12654786c93be1aa79781b09dfd640d69ff
MatteoCastigliego <s314196@studenti.polito.it>
2026-03-13 17:42:40 +0100

    feat: first part completed

--- lab02/film.js
+++ lab02/film.js
@@ -3,19 +3,26 @@ import sqlite from 'sqlite3'
 
 const db = new sqlite.Database('films.db', (err) => {
     if(err) console.log("Database not connected");
-    else console.log("Database connected!");
 });
 
 function Film(filmID, title, favourite = false, date, rating = null, userID = 1){
     this.filmID = filmID;
     this.title = title;
     this.favourite = favourite;
-    this.date = dayjs(date, "YYYY-MM-DD");
+    this.date = dayjs(date);
     this.rating = rating;
     this. userID = userID;
 
     this.printFilm = () => {
         console.log("Id: ", this.filmID, "Title: ", this.title, "Favoutite: ", this.favourite, "Watch Date: ", dayjs(this.date).format('MMMM D, YYYY'), "Rating: ", this.rating, "User id: ", this.userID);
+    }
+}
+
+function print_films(films){
+    for(const film of films){
+    let new_film = new Film(film.id, film.title, film.isFavorite, film.watchDate, film.rating, film.userId)
+    favourite_films.push(new_film);
+    new_film.printFilm();
     }
 }
 
@@ -26,56 +33,77 @@ function Film_Library(){
         return new Promise((resolve, reject) => {
             const query = 'SELECT * FROM films'
             db.all(query, (err, rows) => {
-                if(err) reject("Error durins selection of all elements");
+                if(err) reject("Error during selection of all elements");
                 else resolve(rows);
             })
         })
     };
 
+    this.getALLFavourite = () => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM films WHERE isFavorite = ?';
+            db.all(query, [1], (err, rows) => {
+                if(err) reject("Error during selection of favourite films");
+                else resolve(rows);
+            })
+        })
+    }
+
+    this.getFilmsAfterDate = (date) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM films WHERE watchDate > ?';
+            db.all(query, [date], (err, rows) => {
+                if(err) reject("Error during selection of films after date");
+                else resolve(rows);
+            })
+        })
+    };
+
+    this.getFilmWithString = (string) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM films WHERE title LIKE ?';
+            db.all(query, [`%${string}%`], (err, rows) => {
+                if(err) reject("Error during finding a film title with a given string");
+                else resolve(rows);
+            })
+        })
+    }
+
 }
 
 const film_library = new Film_Library();
+const favourite_films = [];
 
+console.log("\nSELECT ALL FILMS FROM THE DATABASE:")
 film_library.getAllFilms().then(films => {
-    for(const film of films){
-        let favourite = false;
-        if(film.isFavourite === 1) favourite = true;
-        let new_film = new Film(film.id, film.title, favourite, film.watchDate, film.rating, film.userId)
-        film_library.film.push(new_film);
-        new_film.printFilm();
-    }
+    print_films(films);
+}).catch(err => console.log(err))
+.then(() => {
+    console.log("\nSELECT ONLY FAVOURITE FILMS FROM THE DATABASE:")
+    return film_library.getALLFavourite();
 })
+.then(films => {
+    print_films(films);
+})
+.then(() => {
+    const date = "2026-03-12";
+    console.log("\nFILM AFTER DATE: ", date)
+    return film_library.getFilmsAfterDate(date);
+})
+.then(films => {
+    print_films(films);
+})
+.then(() => {
+    console.log("\nFILM WITH A TITLE WITH A GIVEN STRING:");
+    const string = "Star";
+    return film_library.getFilmWithString(string);
+})
+.then(films => {
+    print_films(films);
+}).catch(err => console.log(err));
 
 
 
 
 
-
-/*console.log("sorted film by watch date");
-film_library.film.sort((a, b) => a.date - b.date);
-
-for(const film of film_library.film){
-    film.printFilm();
-}
-
-console.log("sorted film by rating")
-film_library.film.sort((a, b) => b.rating - a.rating);
-
-for(const film of film_library.film){
-    film.printFilm();
-}
-
-console.log("Delete films with id = 1")
-film_library.removeFilm(1);
-
-for(const film of film_library.film){
-    film.printFilm();
-}
-
-console.log("Update rating of film with id 4 from 3 to 1")
-film_library.updateRating(4, 1);
-
-for(const film of film_library.film){
-    film.printFilm();
-}*/
 
