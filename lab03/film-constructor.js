fe54f4bde1aeeaedeaa558350b949ac4f7a7d940
MatteoCastigliego <s314196@studenti.polito.it>
2026-03-26 16:49:28 +0100

    feat: implemented film constructor

---
+++ lab03/film-constructor.js
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+
+export default function Film(id, title, isFavorite = false, rating = null, watchDate, userId) {
+    this.id = id;
+    this.title = title;
+    this.isFavorite = isFavorite;
+    this.rating = rating;
+    // Salva come oggetto dayjs solo se la data è valida
+    this.watchDate = watchDate ? dayjs(watchDate) : null;
+    this.userId = userId;
+}
