cb957f4d0eb353cb3573385163ddb6a926bf72cf
MatteoCastigliego <s314196@studenti.polito.it>
2026-04-18 14:58:52 +0200

    feat: created films, array of films and state for films

--- lab05/first-react-app/src/App.jsx
+++ lab05/first-react-app/src/App.jsx
@@ -9,7 +9,22 @@ import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+  const film1 = new Film(1, "Mare Fuori", true, "10/03/2025", 4, 2);
+  const film2 = new Film(2, "Quo Vado?", true, "14/02/2019", 3, 1);
+  const film3 = new Film(3, "Suits", true, "16/12/2020", 5, 1);
+  const film4 = new Film(4, "Harry Potter", false,"25/05/1991", 3, 3);
+  const film5 = new Film(5, "Benvenuti al Sud", false, "11/01/1974", 3, 4);
+  const film6 = new Film(6, "Fast And Furious", true, "24/07/1975", 4, 2);
   
+  const FilmsList = [];
+  FilmsList.push(film1)
+  FilmsList.push(film2)
+  FilmsList.push(film3)
+  FilmsList.push(film4)
+  FilmsList.push(film5)
+  FilmsList.push(film6)
+
+  const [film, setFilms] = useState(FilmsList)
 
   return (
     <>
@@ -22,7 +37,7 @@ function App() {
             <Filters></Filters>
           </Col>
           <Col md={8}>
-            <ListOfFilms></ListOfFilms>
+            <ListOfFilms film = {film}></ListOfFilms>
           </Col>
         </Row>
       </Container>
